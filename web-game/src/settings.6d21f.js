d1ec94a427d0486c2a010dac0688d992ba0079f7
wushuangabao <445395697@qq.com>
2023-07-05 19:11:01 +0800

    Create web-game

---
+++ web-game/src/settings.6d21f.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Map.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"f1413",resources:"5d21f",main:"b8310"}};
