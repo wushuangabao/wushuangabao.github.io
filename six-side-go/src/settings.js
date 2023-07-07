974807c8d36264c47ea716dfc2f35989ba29962b
wushuangabao <445395697@qq.com>
2023-07-07 16:29:37 +0800

    modify text

--- six-side-go/src/settings.js
+++ six-side-go/src/settings.js
@@ -1,1 +1,1 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/menu.fire",orientation:"portrait",jsList:[]};
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/menu.fire",orientation:"landscape",jsList:[]};
